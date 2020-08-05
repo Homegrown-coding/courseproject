50a06d54fca61f080505f900286cc4d7f9073cdf
Bohdan <example@gmail.com>
2020-08-05 20:24:14 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,99 @@
+"use strict";
+
+/*let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+  a: 50,
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+console.log(name);
+var name = "Bohdan";
+
+{
+  let result = 50;
+}
+
+console.log(result);
+*/
+
+/*
+let number = 4.6;
+
+console.log(-4 / 0);
+console.log("string" * 9);
+
+const person = "5";
+
+const bool = true;
+
+console.log(something);
+
+let und;
+
+console.log(und);
+
+const obj = {
+  name: "John",
+  age: 25,
+  isMarried: false,
+};
+
+console.log(obj.name);
+
+let arr = ['plum.png', 'orange.jpeg', 6,  'apple.bmp', {}, []];
+console.log(arr[1]);
+*/
+
+//alert("Hello");
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = +prompt("Тобі вже є 18?", "18");
+//console.log(answer + 5);
+
+//const answers = [];
+
+//answers[0] = prompt("Як вас звати?", "");
+//answers[1] = prompt("Як ваше прізвище?", "");
+//answers[2] = prompt("Скільки вам років?", "");
+
+//console.log(typeof(answers));
+
+//const category = "toys";
+
+//console.log(`https://someurl.com/${category}/5`);
+
+//const user = "Bohdan";
+
+//alert(`Привіт, ${user}`);
+
+//console.log(4 + +"5");
+
+let incr = 10,
+  decr = 10;
+
+//++incr;
+//--decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5 % 2);
+
+console.log(2 + 2 * 2 !== "6");
+
+const isChecked = false,
+  isClose = false;
+
+//console.log(isChecked && isClose);
+
+console.log(isChecked || !isClose);
